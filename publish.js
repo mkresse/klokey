5b1c919e7f1183bdf18277c98e9b126896986e0b
Sascha Kranz <s.kranz@espresto.com>
2017-03-31 22:27:46 +0200

    - added optional mqtt support -- configurable via config.json -- all events i could find will be published to (sub)topic (see readme.md) - you can now disable hardware stuff (serial and display) via config - you can now disable ssl via config - made the serialport dependency optional, because it does not compile on my osx machine - added small readme.md - wrote 0 tests ¯\_(ツ)_/¯

---
+++ publish.js
@@ -0,0 +1,29 @@
+"use strict";
+
+var mqtt = require('mqtt');
+var _ = require('underscore');
+var client;
+var topic;
+var logger;
+
+exports.init = function(options, logs) {
+    logger = logs;
+    if (! options.enable) {logger.info('publisher disabled'); return}
+
+    // setting some defaults
+    _.extend({
+        host: '127.0.0.1',
+        port: '1883',
+        topic: 'klokey'
+    }, options);
+
+    client = mqtt.connect('mqtt://' + options.host + ':' + options.port);
+    topic = options.topic;
+    logger.info('publisher initialized');
+};
+
+exports.publish = function(message, topicPostfix) {
+    if (! client) {return}
+    if (typeof topicPostfix === 'undefined') { topicPostfix = ''; }
+    client.publish(topic + topicPostfix, message);
+};
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 "use strict";
 
+var EventEmitter =  require('events').EventEmitter;
 var nconf = require('nconf');
 var winston = require('winston');
 var express = require('express');
@@ -17,11 +18,14 @@ var moment = require('moment');
 var nodemailer = require('nodemailer');
 var EmailTemplate = require('email-templates').EmailTemplate;
 var url = require('url');
-
-var sensorModule = require('./sensor.js');
-var display = require('./display.js');
 var anim = require('./anim.js');
 var hipchat = require('./hipchat.js');
+var publisher = require('./publish.js');
+
+nconf.argv().env().file({ file: 'config.json' });
+
+var sensorModule = nconf.get('enableSensor') ? require('./sensor.js') : {init:function() {return new EventEmitter()}};
+var display = nconf.get('enableDisplay') ? require('./display.js') : {init:function(){}};
 
 var logger = new (winston.Logger)({
     transports: [
@@ -44,12 +48,6 @@ var vars = {
 };
 
 moment.locale('de');
-nconf.argv().env().file({ file: 'config.json' });
-
-var serverOptions = {
-    key: fs.readFileSync(nconf.get('privateKey')),
-    cert: fs.readFileSync(nconf.get('certificate'))
-};
 
 var transporter = nodemailer.createTransport(nconf.get('mail').transport);
 var templateDir = path.join(__dirname, 'templates', 'missing');
@@ -59,6 +57,9 @@ var sendMissingMail = transporter.templateSender(new EmailTemplate(templateDir),
         "to": nconf.get('mail').to
     }
 );
+
+publisher.init(nconf.get('mqtt'), logger);
+publisher.publish('STARTED', '/server');
 
 display.init(logger);
 anim.init({interval: 10});
@@ -85,9 +86,8 @@ sensor.on('open', function() {
     }
 });
 
-
 var app = express();
-var server = https.createServer(serverOptions, app);
+var server = initServer(nconf);
 var io = socket(server);
 
 var messages = {
@@ -165,14 +165,17 @@ app.use('/lib', express.static('lib', {
 }));
 
 app.get('/state', function (req, res) {
+    publisher.publish('STATE', '/server');
     res.json(state);
 });
 
 app.get('/test', function (req, res) {
+    publisher.publish('TEST', '/server');
     res.sendFile(__dirname + '/web/test.html');
 });
 
 app.get('/switch', function (req, res) {
+    publisher.publish('SWITCH', '/server');
     if (state.keyPresent) {
         onKeyTaken();
     } else {
@@ -372,6 +375,7 @@ sensor.on('RFIDDataReceived', function() {
 
 function onQueueTimerExpired(clientId) {
     logger.info('timer expired for client: ', clientId);
+    publisher.publish('EXPIRED', '/queue');
 
     internalState.timerQueue = undefined;
     removeFromQueue(clientId);
@@ -382,6 +386,7 @@ function onKeyTaken() {
         logger.warn('illegal state - ignoring taken event');
     } else {
         logger.info('key was TAKEN');
+        publisher.publish('TAKEN', '/state');
 
         state.keyPresent = false;
         internalState.keyTakenOn = new Date();
@@ -402,6 +407,7 @@ function onKeyTaken() {
 
 function onKeyWentMissing() {
     logger.info('key went MISSING');
+    publisher.publish('MISSING', '/state');
 
     state.keyMissing = true;
     state.keyMissingSince = new Date();
@@ -417,6 +423,7 @@ function onKeyWentMissing() {
 
 function onKeyMissingMail() {
     logger.info('sending missing email');
+    publisher.publish('MAIL', '/state');
 
     internalState.timerMissing = undefined;
 
@@ -432,6 +439,7 @@ function onKeyReturned() {
         logger.warn('illegal state - ignoring returned event');
     } else {
         logger.info('key was RETURNED');
+        publisher.publish('RETURNED', '/state');
 
         state.keyPresent = true;
         state.keyMissing = false;
@@ -500,6 +508,7 @@ io.on('connection', function (socket) {
     getConnectionList(clientId).push(socket.id);
 
     logger.info('a client connected from %s (%s)', socket.client.conn.remoteAddress, socket.id);
+    publisher.publish('CONNECT', '/client');
 
     logger.debug('connect headers', socket.request.headers);
 
@@ -522,10 +531,12 @@ io.on('connection', function (socket) {
 
         switch(msg.type) {
             case messages.REQ_ENQUEUE:
+                publisher.publish('ENQUEUE', '/queue');
                 onEnqueueRequest(clientId, replyFn);
                 break;
 
             case messages.REQ_LEAVE_QUEUE:
+                publisher.publish('LEAVE', '/queue');
                 onLeaveQueueRequest(clientId, replyFn);
                 break;
         }
@@ -533,10 +544,12 @@ io.on('connection', function (socket) {
 
     socket.on('close', function() {
         logger.info('a client closed (%s)', socket.id);
+        publisher.publish('CLOSE', '/client');
     });
 
     socket.on('disconnect', function() {
         logger.info('a client disconnected (%s)', socket.id);
+        publisher.publish('DISCONNECT', '/client');
         // delay cleaning up to support browser reload
         setTimeout(function() {
             if (removeFromConnectionList(clientId, socket.id) && !hipchatIntegration.isHipchatUser(clientId)) {
@@ -546,6 +559,19 @@ io.on('connection', function (socket) {
         }, vars.CLEANUP_DELAY);
     });
 });
+
+function initServer(nconf) {
+    if (nconf.get('enableSSL')) {
+        var serverOptions = {
+            key: fs.readFileSync(nconf.get('privateKey')),
+            cert: fs.readFileSync(nconf.get('certificate'))
+        };
+
+        return https.createServer(serverOptions, app)
+    }
+
+    return http.createServer(app);
+}
 
 server.listen(nconf.get('port'), function () {
     var addr = server.address();
